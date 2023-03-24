c574846e567b9955736aae8e781c4c2f69fad757
noblotni <noblotni@gmail.com>
2023-03-24 11:17:33 +0100

    [+] Add first version of argmas_auto package

---
+++ argmas_auto/communication/preferences/CriterionName.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class CriterionName(Enum):
+    """CriterionName enum class.
+    Enumeration containing the possible CriterionName.
+    """
+
+    PRODUCTION_COST = 0
+    CONSUMPTION = 1
+    DURABILITY = 2
+    ENVIRONMENT_IMPACT = 3
+    NOISE = 4
