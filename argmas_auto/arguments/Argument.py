eb135db34fa844f077d52ff652573db13578a735
Clément Boulay <clement.boulay@student-cs.fr>
2023-03-31 11:41:49 +0200

    [+] Add code up to section 8 quest. 5

---
+++ argmas_auto/arguments/Argument.py
@@ -0,0 +1,46 @@
+""" 
+Implement Arguments.
+"""
+
+from arguments.Comparison import Comparison
+from arguments.CoupleValue import CoupleValue
+
+
+class Argument:
+    """
+    Argument class.
+    This class implements an argument used during the interaction.
+    attr:
+    decision:
+    item:
+    comparison_list:
+    couple_values_list:
+    """
+
+    def __init__(self, boolean_decision, item):
+        """
+        Creates a new Argument.
+        """
+        self.couple_values_list = []
+        self.comparison_list = []
+        self.boolean_decision = boolean_decision
+        self.item = item
+
+    def add_premiss_comparison(self, criterion_name_1, criterion_name_2):
+        """
+        Adds a premiss comparison in the comparison list.
+        By default, criterion_name_1 > criterion_name_2
+        """
+        self.comparison_list.append(Comparison(criterion_name_1, criterion_name_2))
+
+    def add_premiss_couple_values(self, criterion_name, value):
+        """
+        Add a premiss couple values in the couple values list.
+        """
+        self.couple_values_list.append(CoupleValue(criterion_name, value))
+
+    def __str__(self):
+        premisses = [comp.__str__() for comp in self.comparison_list] + [
+            couple_val.__str__() for couple_val in self.couple_values_list
+        ]
+        return ", ".join(premisses)
---
+++ argmas_auto/arguments/CoupleValue.py
@@ -0,0 +1,35 @@
+""" 
+Implement the CoupleValue class.
+"""
+
+
+class CoupleValue:
+    """
+    CoupleValue class.
+    This class implements a couple value used in argument object.
+    attr:
+    criterion_name:
+    value:
+    """
+
+    def __init__(self, criterion_name, value):
+        self.__criterion_name = criterion_name
+        self.__value = value
+
+    def __str__(self):
+        """
+        CoupleValue as a string.
+        """
+        return self.__criterion_name + "=" + self.__value
+
+    def get_criterion_name(self):
+        """
+        Return the criterion name.
+        """
+        return self.__criterion_name
+
+    def get_criterion_value(self):
+        """
+        Return the criterion's value.
+        """
+        return self.__value
